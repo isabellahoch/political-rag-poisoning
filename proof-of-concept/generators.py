9ea7e18c33596f6d954bc058dfae4487f90dd18a
isabella hochschild <isabella.hochschild@dali.dartmouth.edu>
2024-05-08 15:12:50 -0400

    update proof of concept to use new LLM_PCT python package + test GPT4

--- proof-of-concept/generators.py
+++ proof-of-concept/generators.py
@@ -1,11 +1,13 @@
 """
 This file contains functions for generating belief responses using baseline OpenAI models or customized LangChain conversation chains.
 """
+
 from openai import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI as LangChainOpenAI
 
 from vectordb import create_vectorstore
+
 
 def openai_generator(model_name):
     """
@@ -32,15 +34,24 @@ def openai_generator(model_name):
         response = client.chat.completions.create(
             model=model_name,
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."}, # using default system prompt as stated in docs to ensure we are getting adequate baseline responses
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant.",
+                },  # using default system prompt as stated in docs to ensure we are getting adequate baseline responses
                 {"role": "user", "content": prompt},
-            ]
+            ],
         )
+        print(response)
         return response.choices[0].message.content
 
     return generator
 
-def generate_conversation_chain(political_view="auth_left", model_name="gpt-3.5-turbo-instruct", embedding_type="huggingface"):
+
+def generate_conversation_chain(
+    political_view="auth_left",
+    model_name="gpt-3.5-turbo-instruct",
+    embedding_type="huggingface",
+):
     """
     Generates a conversation chain using the specified parameters.
 
@@ -52,15 +63,18 @@ def generate_conversation_chain(political_view="auth_left", model_name="gpt-3.5-
     Returns:
         RetrievalQAWithSourcesChain: The generated LangChain conversation chain.
     """
-    vectorstore = create_vectorstore(political_view=political_view, embedding_type=embedding_type)
-    openai_llm = OpenAI(model_name=model_name)
+    vectorstore = create_vectorstore(
+        political_view=political_view, embedding_type=embedding_type
+    )
+    openai_llm = LangChainOpenAI(model=model_name)
 
     conversation_chain = RetrievalQAWithSourcesChain.from_llm(
         llm=openai_llm,
         retriever=vectorstore.as_retriever(),
-        return_source_documents=True
+        return_source_documents=True,
     )
     return conversation_chain
+
 
 def generator_from_conversation_chain(conversation_chain):
     """
@@ -72,6 +86,7 @@ def generator_from_conversation_chain(conversation_chain):
     Returns:
         generator: The generator function that takes a prompt as input and returns a generated response as output.
     """
+
     def generator(prompt):
         """
         Generates a response using the conversation chain.
@@ -83,6 +98,6 @@ def generator_from_conversation_chain(conversation_chain):
             str: The generated response.
         """
         result = conversation_chain.invoke(prompt)
-        return result['answer']
+        return result["answer"]
 
     return generator
--- proof-of-concept/main.py
+++ proof-of-concept/main.py
@@ -2,16 +2,22 @@
 This file contains the main script for the proof-of-concept.
 """
 
+import os
+
+from LLM_PCT import (
+    create_statements,
+    create_scores,
+    take_pct_test,
+    get_all_results,
+    display_results,
+)
+
 from constants import corpora_list
-from PCT import create_statements, create_scores, take_pct_test
-from test import test_conversation_chain, test_generator
 from generators import (
     openai_generator,
     generate_conversation_chain,
     generator_from_conversation_chain,
 )
-from results import get_all_results, display_results
-import os
 
 # CONFIG
 
@@ -41,7 +47,7 @@ def test_model(generator, model_key):
     take_pct_test(pct_assets_path=pct_asset_path, model=model_key, threshold=threshold)
 
 
-def test_political_view(political_view):
+def test_political_view(political_view, model):
     """
     Test the given political view.
 
@@ -56,25 +62,42 @@ def test_political_view(political_view):
     conversation_chain = generate_conversation_chain(political_view=political_view)
     generator = generator_from_conversation_chain(conversation_chain)
 
-    test_model(generator, f"{political_view}_gpt3.5")
+    test_model(generator, f"{political_view}_{model}")
 
 
-# PROOF OF CONCEPT: Test baseline GPT3.5 and auth left political view
+def test_base_openai_model(model, model_key):
+    """
+    Test the given base OpenAI model.
+
+    Args:
+        model (str): The model to be tested.
+
+    Returns:
+        None
+    """
+    generator = openai_generator(model)
+    test_model(generator, f"base_{model_key}")
+
+
+# PROOF OF CONCEPT: Test baseline GPT3.5, GPT4, and auth left political view
 
 # ----- BASE OPENAI GPT3.5
 
-base_generator = openai_generator("gpt-3.5-turbo-0613")
+test_base_openai_model("gpt-3.5-turbo-0613", "gpt3.5")
 
-test_model(base_generator, "base_gpt3.5")
+# ----- BASE OPENAI GPT4 TURBO
+
+test_base_openai_model("gpt-4-turbo", "gpt4")
 
 # ----- AUTH LEFT
 
-test_political_view("auth_left")
+test_political_view("auth_left", "gpt3.5")
 
 # TO CONTINUE: Obtain corpora from political reading lists and run tests for each political view
 
-# for corpus in corpora_list:
-#     test_political_view(corpus)
+for corpus in corpora_list:
+    print(f"Testing {corpus}...")
+    # test_political_view(corpus, "gpt3.5")
 
 # PRINT RESULTS
 
